08cd2b5f795f753e12f58f8587dff29e34447462
timdows <tim@inexpro.nl>
2017-02-25 17:45:28 +0100

    Implement togglable aside

--- src/app/side-menu/side-menu.component.ts
+++ src/app/side-menu/side-menu.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Inject, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { DOCUMENT } from '@angular/platform-browser';
 
 @Component({
 	selector: 'app-side-menu',
@@ -7,8 +8,9 @@ import { Router } from '@angular/router';
 	styleUrls: ['./side-menu.component.scss']
 })
 export class SideMenuComponent implements OnInit {
+	isOpenAside = true;
 
-	constructor(private router: Router) { }
+	constructor(@Inject(DOCUMENT) private document: any, private router: Router) { }
 
 	ngOnInit() {
 	}
@@ -17,4 +19,14 @@ export class SideMenuComponent implements OnInit {
 		this.router.navigate(["/" + type]);
 	}
 
+	toggleAside(){
+		this.isOpenAside = !this.isOpenAside;
+
+		if(this.document.querySelector('html').classList.contains("aside-open")){
+			this.document.querySelector('html').classList.remove("aside-open");
+		}
+		else{
+			this.document.querySelector('html').classList.add("aside-open");
+		}
+	}
 }
