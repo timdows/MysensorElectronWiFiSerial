18e78e1f374c04dfd87b3ad51ea2a5e48eb6de29
timdows <tim@inexpro.nl>
2017-03-21 17:55:58 +0100

    Enable change detection

--- src/app/gateway-serial/gateway-serial.component.ts
+++ src/app/gateway-serial/gateway-serial.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ChangeDetectorRef } from '@angular/core';
 import { Subscription } from 'rxjs/Subscription';
 import { SerialDataService } from '../serial-data.service';
 
@@ -12,13 +12,16 @@ export class GatewaySerialComponent implements OnInit {
 	subscription: Subscription;
 	data = [];
 
-	constructor(private serialDataService: SerialDataService) { }
+	constructor(
+		private changeDetectorRef: ChangeDetectorRef,
+		private serialDataService: SerialDataService) { }
 
 	ngOnInit() {
 		this.subscription = this.serialDataService
 			.getAsObservable()
 			.subscribe(() => { 
 				this.data = this.serialDataService.getData();
+				this.changeDetectorRef.detectChanges();
 			});
 	}
 }
