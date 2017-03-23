48226d8e22e189f544b8a0762ddeba26bd93dd19
timdows <tim@inexpro.nl>
2017-03-23 13:08:57 +0100

    Display heating graphs

--- src/app/heating-stats/heating-stats.component.ts
+++ src/app/heating-stats/heating-stats.component.ts
@@ -10,15 +10,15 @@ declare var d3: any;
 })
 export class HeatingStatsComponent implements OnInit {
 
-	clientModel = {};
+	clientModel = [];
 
 	options = {
 		chart: {
-			type: 'discreteBarChart',
+			type: 'multiBarChart',
 			height: 450,
 			margin: {
 				top: 20,
-				right: 20,
+				//right: 20,
 				bottom: 50,
 				left: 55
 			},
@@ -29,55 +29,15 @@ export class HeatingStatsComponent implements OnInit {
 				return d3.format(',.4f')(d);
 			},
 			duration: 500,
-			xAxis: {
-				axisLabel: 'X Axis'
-			},
+			// xAxis: {
+			// 	axisLabel: 'X Axis'
+			// },
 			yAxis: {
-				axisLabel: 'Y Axis',
+				axisLabel: 'Ticks',
 				axisLabelDistance: -10
 			}
 		}
 	}
-
-	data = [
-		{
-			key: "Cumulative Return",
-			values: [
-				{
-					"label": "A",
-					"value": -29.765957771107
-				},
-				{
-					"label": "B",
-					"value": 0
-				},
-				{
-					"label": "C",
-					"value": 32.807804682612
-				},
-				{
-					"label": "D",
-					"value": 196.45946739256
-				},
-				{
-					"label": "E",
-					"value": 0.19434030906893
-				},
-				{
-					"label": "F",
-					"value": -98.079782601442
-				},
-				{
-					"label": "G",
-					"value": -13.925743130903
-				},
-				{
-					"label": "H",
-					"value": -5.1387322875705
-				}
-			]
-		}
-	];
 
 	constructor(private httpService: HttpService) { }
 
@@ -86,7 +46,7 @@ export class HeatingStatsComponent implements OnInit {
 	}
 
 	private getClientModel() {
-		this.httpService.get("http://localhost:5002/heater/getclientmodel.json?heaterMeterGroupID=1")
+		this.httpService.get("http://localhost:5002/heater/getclientmodel.json")
 			.subscribe(data => {
 				this.clientModel = data.json();
 			});
