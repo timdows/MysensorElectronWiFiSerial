1cab9cdfc07b300d5da662b71f2f2b44fc1ed962
timdows <tim@inexpro.nl>
2017-04-01 13:48:32 +0200

    Add time to sidemenu

--- src/app/side-menu/side-menu.component.ts
+++ src/app/side-menu/side-menu.component.ts
@@ -1,18 +1,23 @@
-import { Component, Inject, OnInit, ChangeDetectorRef } from '@angular/core';
+import { Component, Inject, OnInit, ChangeDetectorRef, OnDestroy } from '@angular/core';
 import { Router } from '@angular/router';
 
 import { IpcService, EnvironmentService } from '../_services/index';
+import { TimerObservable } from "rxjs/observable/TimerObservable";
+import { Subscription } from "rxjs/Subscription";
 
 @Component({
 	selector: 'app-side-menu',
 	templateUrl: './side-menu.component.pug',
 	styleUrls: ['./side-menu.component.scss']
 })
-export class SideMenuComponent implements OnInit {
+export class SideMenuComponent implements OnInit, OnDestroy {
 
 	info = {};
 	ipAddressInfo = [];
 	isOpenAside = true;
+
+	private subscription: Subscription;
+	timeString = new Date().toLocaleTimeString();
 
 	constructor(
 		private ipcService: IpcService,
@@ -21,17 +26,26 @@ export class SideMenuComponent implements OnInit {
 		private router: Router) { }
 
 	ngOnInit() {
+		let timer = TimerObservable.create(2000, 1000);
+		this.subscription = timer.subscribe(t => {
+			this.timeString = new Date().toLocaleTimeString();
+		});
+
 		setTimeout(() => {
 			this.ipcService.subscribeToEvent("set-os-content", this, this.handleSetOsContent);
 			this.ipcService.getOsContent();
 		}, 0);
 	}
 
-	changeRoute(type: string){
+	ngOnDestroy() {
+		this.subscription.unsubscribe();
+	}
+
+	changeRoute(type: string) {
 		this.router.navigate(["/" + type]);
 	}
 
-	toggleAside(){
+	toggleAside() {
 		if (this.isOpenAside) {
 			this.environmentService.removeClassOnHtml("aside-open");
 		}
@@ -42,10 +56,10 @@ export class SideMenuComponent implements OnInit {
 		this.isOpenAside = !this.isOpenAside;
 	}
 
-	handleSetOsContent(info: any) {	
+	handleSetOsContent(info: any) {
 		this.info = info;
 		this.ipAddressInfo = [];
-		
+
 		Object.keys(info.networkInterfaces).forEach((ifname) => {
 			var alias = 0;
 			info.networkInterfaces[ifname].forEach((iface) => {
