d96c50c596fb316a07f57f2483f900bf7341ff18
timdows <tim@inexpro.nl>
2017-04-25 23:12:13 +0200

    Upgrade fixes

--- builder/build.sh
+++ builder/build.sh
@@ -4,7 +4,7 @@ cd "$(dirname "$0")"
 git reset --hard
 git pull
 
-#cp common.js ../node_modules/@angular/cli/models/webpack-configs/common.js
+cp common.js ../node_modules/@angular/cli/models/webpack-configs/common.js
 cp .env ../.env
 
 cd ..
